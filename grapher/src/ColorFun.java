1d13105620d9a9a3021f0b8cb9d224155f7f89a4
chris <chriskjellqvis@gmail.com>
2016-06-14 13:56:22 -0500

    Cleaned up, More comments. A little clearer.

--- grapher/src/ColorFun.java
+++ grapher/src/ColorFun.java
@@ -1,29 +1,38 @@
-import javax.swing.*;
 import java.awt.*;
+
 public class ColorFun
-/** Rory O'Dwyer with the aid of Chris Kjellqvist. Math concepts borrowed from desmos calculator project 12.6.15
- * Please reference all the creators of this project if code is borrowed.
+/**
+ * @author Chris Kjellqvist and Rory O'Dwyer
+ * Generates color shadings and combines RGB colors.
  */
 {
-    public static int numberOfColors = 10000;
-    public static float[] color1 = Color.RGBtoHSB(130, 255, 255, null);
-    public float[] color2 = Color.RGBtoHSB(0, 0, 0, null);
-    public Color[] colorArray = getColorPallete(numberOfColors, color1, color2);
-    public ColorFun(){
+    public ColorFun() {
     }
-    public Color[] getColorPallete(int n, float[] c1, float[] c2){
-            Color[] ar = new Color[n];
+
+    /**
+     * Gives a smooth gradient of n samples from RGB c1 to RGB c2
+     * @param n - number of samples
+     * @param c1 - color to go from
+     * @param c2 - color to go to
+     * @return - Gradient of colors
+     */
+    public static Color[] getColorPallete(int n, float[] c1, float[] c2) {
+        Color[] ar = new Color[n];
         for (int i = 0; i < n; i++) {
-            //How much of color1 and how much of color2? Depends on the ratio
-                       //you tell the combineHSB method to mix them. In this case, it is
-            // (float) i/n.
             float[] temp = combineHSB(c1, c2, (float) i / n);
-            //Convert the HSB value back into RGB so you can use it.
             ar[i] = new Color(Color.HSBtoRGB(temp[0], temp[1], temp[2]));
         }
         return ar;
     }
-        private static float[] combineHSB(float[] c1, float[] c2, float ratio) {
+
+    /**
+     * Combines two HSB colors in ratio
+     * @param c1 - color 1
+     * @param c2 - color 2
+     * @param ratio - ratio by which to combine
+     * @return - combined HSB color
+     */
+    private static float[] combineHSB(float[] c1, float[] c2, float ratio) {
         float[] toReturn = new float[3];
         for (int i = 0; i < 3; i++) {
             toReturn[i] = c1[i] * (1 - ratio) + c2[i] * (ratio);
--- grapher/src/Point.java
+++ grapher/src/Point.java
@@ -1,15 +1,12 @@
-
 /**
-  Rory O'Dwyer with the aid of Chris Kjellqvist. Math concepts borrowed from desmos calculator project 12.6.15
-  Please reference all the creators of this project if code is borrowed.
+ * @author Chris Kjellqvist
  */
-public class Point<T>
-{
-    //The side brackets determine what type it ought to be
+public class Point<T> {
     T x;
     T y;
-    public Point(T x, T y){
-        this.x=x;
-        this.y=y;
+
+    public Point(T x, T y) {
+        this.x = x;
+        this.y = y;
     }
 }
